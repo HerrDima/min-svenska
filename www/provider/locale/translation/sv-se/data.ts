be5ba51090177caf68ac20d177b585d71e738545
HerrDima <dzmitry.turautsou@gmail.com>
2023-01-08 17:42:13 +0100

    bug fixes

--- www/provider/locale/translation/sv-se/data.ts
+++ www/provider/locale/translation/sv-se/data.ts
@@ -9,6 +9,9 @@ export const svSe: LocaleDictionaryType = {
 
     NAVIGATION__HOME: 'Hem',
 
+    GDPR__WE_USE_COOKIES: 'Genom att använda denna webbplats samtycker du till användningen av cookies.',
+    GDPR__WHAT_ARE_COOKIES: 'Vad är cookies?',
+
     SHARE__RECOMMEND_TO_FRIENDS__HEADER: 'Rekommendera till vänner:',
     SHARE__HEADER: 'Dela med sig:',
     SEE_ALSO__HEADER: 'Se även:',
